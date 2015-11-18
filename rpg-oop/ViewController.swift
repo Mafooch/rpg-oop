db62e0b3df4df7d889db9b9fae9845636685a7f5
Joshua Maffuccio <joshua.maffuccio@raizlabs.com>
2015-11-18 16:50:02 -0500

    complete oop project

--- rpg-oop/ViewController.swift
+++ rpg-oop/ViewController.swift
@@ -21,13 +21,64 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var chestBtn: UIButton!
     
+    var player: Player!
+    var enemy: Enemy!
+    //    we know we're gonna assign
+    var chestMessage: String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        player = Player(name: "DirtyLaundry21", hp: 110, attackPwr: 20)
+        
+        generateRandomEnemy()
+        
+        playerHpLbl.text = "\(player.hp) HP"
+    }
+    
+    func generateRandomEnemy() {
+        let rand = Int(arc4random_uniform(2))
+//        saying give me a random number between 0 and 1. two total options. 2 is exclusive
+        
+        if rand == 0 {
+            enemy = Kimara(startingHp: 50, attackPwr: 12)
+//            even though enemy var is of type Enemy it lets us put a kimara because it inherits from enemy and is a type of enemy!
+        } else {
+            enemy = DevilWizard(startingHp: 60, attackPwr: 15)
+        }
+        
+        enemyImg.hidden = false
     }
 
     @IBAction func onChestTapped(sender: AnyObject) {
+        chestBtn.hidden = true
+        printLbl.text = chestMessage
+        NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: "generateRandomEnemy", userInfo: nil, repeats: false)
+//        allow time before next enemy is generated
     }
 
+    @IBAction func attackTapped(sender: AnyObject) {
+        if enemy.attemptAttack(player.attackPwr) {
+            printLbl.text = "Attacked \(enemy.type) for \(player.attackPwr) HP"
+            enemyHpLbl.text = "\(enemy.hp) HP"
+        } else {
+            printLbl.text = "Attack was unsuccessful"
+        }
+        
+        if let loot = enemy.dropLoot() {
+            player.addItemToInventory(loot)
+//            loot could return a string or return nothing. if we get to the below line it means it successfully returned a string
+            chestMessage = "\(player.name) found \(loot)"
+            chestBtn.hidden = false
+        }
+        
+        if !enemy.isAlive {
+            enemyHpLbl.text = ""
+            printLbl.text = "Killed \(enemy.type)"
+            enemyImg.hidden = true
+        }
+    }
 
 }
 
